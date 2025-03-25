e6b734cc11c5012dd977445ce21bd0f8d5272a10
zain <voididentity78@gmail.com>
2025-03-25 15:40:03 -0700

    styled, input reaarranged

--- code/javascript.js
+++ code/javascript.js
@@ -1,24 +1,47 @@
-const program = document.querySelector('#prog')
+const program = []
+const programDisplay = document.querySelector('#prog')
 const runButton = document.querySelector('#run_button')
+const output = document.querySelector('#output')
+const hadamardButton = document.querySelector('#H')
+const paulixButton = document.querySelector('#X')
+const pauliyButton = document.querySelector('#Y')
 let qubit = {
   val: 0
 }
 function runGate(type, qubit) {
   val = qubit.val
   if (type == 'x' || type == 'y') {
-    if (typeof val == Number) {
-      console.log(Math.abs(val - 1))
-    } 
+    qubit.val = Math.abs(val - 1)  }
+
+  if (type == 'h') {
+    if (Math.random()>.5) {
+      qubit.val = 1
+    } else {
+      qubit.val = 0
+    }
   }
+
 }
 
 function runScript() {
-  progString = program.value.split(',')
-  for (const gate in progString) {
-    runGate(gate,qubit)
+  
+  for (const gate in program) {
+    runGate(program[gate],qubit)
   }
+  output.textContent = qubit.val
 }
+
+function appendInput(val) {
+  program.push(val)
+  programDisplay.textContent = program.toString()
+}
+hadamardButton.addEventListener('click', function(){ appendInput('h') })
+paulixButton.addEventListener('click', function(){ appendInput('x') })
+pauliyButton.addEventListener('click', function(){ appendInput('y') })
+
+
 runButton.addEventListener('click', runScript)
-// qubit.val = runGgate('x', qubit)
+
+// qubit.val = runGate('x', qubit)
 
 
